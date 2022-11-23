9f23e8b8854814191bcd1f245413439ef6daf277
oks404 <ksunya2004@list.ru>
2022-11-23 21:12:28 +1000

    Задание 1-4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-
+//  Задание 1
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+//  Задание 2
+        for (int a = 10; a >= 1; a--){
+            System.out.println(a);
+        }
+//  Задание 3
+        for (int b = 0; b <= 17; b = b + 2){
+            System.out.println(b);
+        }
+//  Залание 4
+        for (int c = 10; c >= -10; c--){
+            System.out.println(c);
+        }
 
     }
 }
