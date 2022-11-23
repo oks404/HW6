6bd8963a74b6758c4284f7a6f39a15147758fce6
oks404 <ksunya2004@list.ru>
2022-11-23 21:21:36 +1000

    Задание 1.1 - 2.3

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-//  Задание 1
+//  Задание 1.1
         for (int i = 1; i <= 10; i++){
             System.out.println(i);
         }
-//  Задание 2
+//  Задание 1.2
         for (int a = 10; a >= 1; a--){
             System.out.println(a);
         }
-//  Задание 3
+//  Задание 1.3
         for (int b = 0; b <= 17; b = b + 2){
             System.out.println(b);
         }
-//  Залание 4
+//  Залание 1.4
         for (int c = 10; c >= -10; c--){
             System.out.println(c);
         }
+//  Задание 2.1
+        for (int d = 1904; d <= 2096; d = d +4){
+            System.out.println(d + " год является високосным");
+        }
+//  Задание 2.2
+        for (int e = 7; e <= 98; e = e +7){
+            System.out.println(e);
+        }
+//  Задание 2.3
+        for (int f = 1; f <= 512; f = f * 2){
+            System.out.println(f);
+        }
+
 
     }
 }
